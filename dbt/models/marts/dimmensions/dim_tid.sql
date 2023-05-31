764f0c2df49e8e8546bf71fbe38945359cbf028d
Patrick Pedersen <patrick.pedersen@nav.no>
2023-05-31 10:07:02 +0200

    Add dim_tid from vdl-regnskapsdata

---
+++ dbt/models/marts/dimmensions/dim_tid.sql
@@ -0,0 +1,58 @@
+{{
+    config(
+        materialized='table',
+        cluster_by = ['dato','dim_nivaa']
+    )
+}}
+
+with
+
+dim_tid as (
+  select * from {{ ref('stg_dvh__dim_tid') }}
+),
+
+last_year as (
+  select
+    pk_dim_tid,
+    dateadd('year',-1,dato) as fjor_dato,
+    aar_uke-100 as fjor_aar_uke,
+    aar_maaned-100 as fjor_aar_maaned,
+    aar_kvartal-100 as fjor_aar_kvartal,
+    aar_tertial-100 as fjor_aar_tertial,
+    aar_halvaar-100 as fjor_aar_halvaar,
+    aar-1 as fjor_aar
+  from
+    dim_tid
+),
+
+tertial as (
+  select
+    pk_dim_tid,
+    case tertial 
+      when 1 then '1. tertial' 
+      when 2 then '2. tertial'
+      when 3 then '3. tertial'
+      else 'ukjent'
+    end as tertial_besk
+  from
+    dim_tid
+),
+
+final as (
+  select
+    dim_tid.*,
+    last_year.fjor_dato,
+    last_year.fjor_aar_maaned,
+    last_year.fjor_aar_kvartal,
+    last_year.fjor_aar_tertial,
+    last_year.fjor_aar_halvaar,
+    last_year.fjor_aar,
+    tertial.tertial_besk
+  from dim_tid
+  left join last_year on
+    dim_tid.pk_dim_tid = last_year.pk_dim_tid
+  left join tertial on
+    dim_tid.pk_dim_tid = tertial.pk_dim_tid
+)
+
+select * from final
